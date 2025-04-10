26e6f7c779fd1e0011658ecb492b4cb0befbd644
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-04-11 01:13:52 +0200

    Auth0 Configure to work with login button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Login from './components/Login';
 
 function App() {
   return (
-    <>
+    <Auth0Provider
+      domain={"dev-0guwahdjkljcq658.us.auth0.com"}
+      clientId={"BViyv7wTuGcwpJVyyPcWcyRcRjpmeHJL"}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
       <Login />
-    </>
+    </Auth0Provider>
   );
 }
 
