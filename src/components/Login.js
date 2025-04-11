df2e24f58eb874d666b593710ce264b06ef8ea53
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-04-11 20:32:36 +0200

    Added new buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,13 +17,20 @@ const Login = () => {
   return (
     <>
       {/* Navbar */}
-      <Box sx={{ backgroundColor: '#f5f5f5', display: 'flex', justifyContent: 'flex-end', p: 2 }}>
+      <Box sx={{ backgroundColor: '#f5f5f5', display: 'flex', justifyContent: 'flex-end', p: 2, alignItems: 'center' }}>
+        <Typography sx={{ fontWeight: 'bold', justifyContent: 'center', alignItems: 'center', mr: 2}}>Help us Grow </Typography>
+        <Button 
+          variant="contained" 
+          sx={{ backgroundColor: '#000', color: '#fff', mr: 2 }}
+        >
+          Contact Us
+        </Button>
         <Button 
           variant="contained" 
           onClick={() => loginWithRedirect()}
           sx={{ backgroundColor: '#000', color: '#fff' }}
         >
-          Login as Admin to upload
+          Login as Admin to upload files
         </Button>
       </Box>
 
@@ -31,9 +38,9 @@ const Login = () => {
       <Box sx={{ minHeight: "90vh", display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
         <Box component={'img'} src={logo} alt='logo' sx={{ width: '100px', height: '100px', mb: 2 }} />
         <Typography variant='h4' sx={{ fontWeight: 'bold', mb: 2 }}>Welcome to ConstiFind</Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center', width: '60%', px: 2, py: 2, borderRadius: '10px', border: '1px solid gray', backgroundColor: '#fff' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', width: '70%', px: 2, py: 2, borderRadius: '10px', border: '1px solid gray', backgroundColor: '#fff' }}>
           <SearchIcon sx={{ color: '#000', fontSize: '2rem', mr: 1 }} />
-          <Box component={'input'} sx={{ border: 'none', "&:focus": { outline: 'none' }, flex: 1, fontSize: '18px' }} />
+          <Box component={'input'} sx={{ border: 'none', "&:focus": { outline: 'none' }, flex: 1, fontSize: '16px' }} />
         </Box>
         <Button variant='contained' sx={{ backgroundColor: '#000', color: '#fff', fontSize: '18px', px: 4, py: 1, mt: 2 }}>Search</Button>
       </Box>
