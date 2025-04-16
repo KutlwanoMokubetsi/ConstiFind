2668c91f2dea30eb35b257ae0c089ff319c5ee47
Moloko Tsiri <sylvestertsiri@gmail.com>
2025-04-16 17:07:21 +0200

    Super Admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AdminPage from "./pages/AdminPage";
+import SuperAdminPage from "./pages/SuperAdminPage"; // Add this import
 import SearchPage from "./pages/SearchPage";
 import Navbar from "./components/Navbar";
 import APIDocumentation from "./pages/APIDocumentation";
@@ -13,17 +14,26 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50 flex flex-col">
         <Navbar />
-        <div className="flex-1 w-full"> {/* This will make content take remaining space */}
+        <div className="flex-1 w-full">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route
-          path="/admin"
-          element={
-            <PrivateRoute>
-              <AdminPage />
-            </PrivateRoute>
-          }
-        />
+              path="/admin"
+              element={
+                <PrivateRoute>
+                  <AdminPage />
+                </PrivateRoute>
+              }
+            />
+            {/* Add the new SuperAdmin route */}
+            <Route
+              path="/super-admin"
+              element={
+                <PrivateRoute>
+                  <SuperAdminPage />
+                </PrivateRoute>
+              }
+            />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/api-docs" element={<APIDocumentation />} />
           </Routes>
